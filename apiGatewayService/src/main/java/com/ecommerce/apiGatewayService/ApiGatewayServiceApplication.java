b87250ad6b2cf3faac382f4fc18e42d066e3aa54
Ashish Kumar Singh <djdaydreamer.singh4@gmail.com>
2025-01-14 21:35:52 +0530

    integrating api gateway

--- apiGatewayService/src/main/java/com/ecommerce/apiGatewayService/ApiGatewayServiceApplication.java
+++ apiGatewayService/src/main/java/com/ecommerce/apiGatewayService/ApiGatewayServiceApplication.java
@@ -1,11 +1,12 @@
 package com.ecommerce.apiGatewayService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
--- apiGatewayService/src/main/java/com/ecommerce/apiGatewayService/config/GatewayConfig.java
+++ apiGatewayService/src/main/java/com/ecommerce/apiGatewayService/config/GatewayConfig.java
@@ -1,55 +1,25 @@
 package com.ecommerce.apiGatewayService.config;
 
-
-import com.netflix.eureka.ServerRequestAuthFilter;
-import org.apache.catalina.filters.RequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.WebFilter;
 
 @Configuration
 public class GatewayConfig {
 
-    @Autowired
-    RequestFilter requestFilter;
-
-    @Autowired
-    ServerRequestAuthFilter serverRequestAuthFilter;
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        // adding 2 rotes to first microservice as we need to log request body if method is POST
         return builder.routes()
-                .route("first-microservice", r -> r.path("/first")
-                        .and().method("POST")
-                        .and().readBody(Student.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
-                        .uri("http://localhost:8081"))
-                .route("first-microservice", r -> r.path("/first")
-                        .and().method("GET").filters(f -> f.filters(authFilter))
-                        .uri("http://localhost:8081"))
-                .route("second-microservice", r -> r.path("/second")
-                        .and().method("POST")
-                        .and().readBody(Company.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
-                        .uri("http://localhost:8082"))
-
-                .route("second-microservice", r -> r.path("/second")
-                        .and().method("GET").filters(f -> f.filters(authFilter))
-                        .uri("http://localhost:8082"))
-                .route("auth-server", r -> r.path("/login")
-                        .uri("http://localhost:8088"))
+                .route("auth-service", r -> r
+                        .path("/auth/**")
+                        .uri("lb://auth-service"))
+                .route("user-service", r -> r
+                        .path("/users/**")
+                        .uri("lb://user-service"))
+                .route("order-service", r -> r
+                        .path("/orders/**")
+                        .uri("lb://order-service"))
                 .build();
     }
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public WebFilter responseFilter() {
-        return new PostGlobalFilter();
-    }
-
 }
